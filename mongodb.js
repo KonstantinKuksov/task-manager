367e9d6d311955cfdfc9863ab111da17091cf9e5
Konstantin Kuksov <kkuksov@gmail.com>
2021-03-14 16:07:38 +0200

    work commit

--- mongodb.js
+++ mongodb.js
@@ -13,38 +13,21 @@ MongoClient.connect(
 
     const db = client.db(databaseName);
 
-    // db.collection('users').findOne({ _id: new ObjectID("604cbcc68f08d92890ec0034") }, (error, user) => {
-    //   if (error) {
-    //     return console.log('Unable to fetch');
-    //   }
-    //   console.log(user);
+    // db.collection('users').deleteMany({
+    //   age: 37,
+    // }).then( (result) => {
+    //   console.log(result);
+    // }).catch( (error) => {
+    //   console.log(error);
     // });
 
-    // db.collection('users').find({age: 37}).toArray((error, users) => {
-    //   if (error) {
-    //     console.log('Unable to fetch');
-    //   }
-    //   console.log(users);
-    // });
-
-    // db.collection('users').find({age: 37}).count((error, count) => {
-    //   if (error) {
-    //     console.log('Unable to fetch');
-    //   }
-    //   console.log(count);
-    // });
-
-    db.collection('tasks').findOne({_id: new ObjectID("604cbc3980c96e15cc65f2bf")},  (error, task) => {
-      if (error) {
-        console.log('Unable to fetch');   
-      }
-      console.log(task);
-    });
-
-    db.collection('tasks').find({completed: false}).toArray((error, tasks) => {
-      console.log();
-      console.log('Not completed tasks:');
-      console.log(tasks);
-    });
+    db.collection('tasks')
+      .deleteOne({ description: 'take a lunch' })
+      .then((result) => {
+        console.log(result);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   }
 );
